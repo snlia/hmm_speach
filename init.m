60c923518fc9733e13dde76b7874826a13d2aa3c
snlia <chinsnlia@gmail.com>
2016-06-19 15:11:38 +0800

    增加了一些...注释？

--- init.m
+++ init.m
@@ -4,12 +4,12 @@ function [ignore] = init
 
 ignore = [];
 
-global N; % 单词数量
-N = 1; 
-global M; % 采样数
-M = 20; 
+global theN; % 单词数量
+theN = 1; 
+global theM; % 采样数
+theM = 20; 
 
-global words; %单词集
-words = cell (1,N); 
-words = {'Happy'};
+global theWords; %单词集
+theWords = cell (1,N); 
+theWords = {'Happy'};
 
--- vad.m
+++ vad.m
@@ -1,3 +1,13 @@
-%函数输入一个时域采样信息y与采样频率fs，返回发音部分的起点与终点。
+%起/终点判断函数
+%输入一个时域采样信息y与采样频率fs，返回发音部分的起点与终点。
 function [startp, endp] = vad (y, fs)
+
+%设定参数
+framlen = fs / 100; % 10ms
+framinc = framlen * 0.5; % 桢长一半
+
+%幅度归一化到[-1,1]
+y = double(y);
+max_y = max(abs(y));
+y = y / max_y;
 
