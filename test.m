b836af645d1d6a89410bc2cc35ac287d0a9d54a9
snlia <chinsnlia@gmail.com>
2016-06-20 12:42:59 +0800

    已经可以大体上判断起末位置，但是因为无法正确区分清音部分和无声部分，需要进一步调试！ 测试用	新文件：   test.m

---
+++ test.m
@@ -0,0 +1,23 @@
+%-------全局变量设定------
+global theN; % 单词数量
+global theM; % 采样数
+global theWords; %单词集
+%-------全局变量设定------
+disp ('初始化参数...');
+ignore = init;
+disp ('分析各个单词的模板信息...');
+for i = (1:theN)
+    for j = (1:theM)
+        [y, fs] = readwav (char (theWords (i)), int2str (j));
+        [startp, endp] = vad (y, fs);
+        subplot(3,1,3);
+        plot(y);
+        axis([1,length(y),min(y),max(y)]);
+        line([startp ,startp],[min(y),max(y)],'color','red');
+        line([endp ,endp],[min(y),max(y)],'color','red');
+        disp('显示端点……');
+        sound (y(startp:endp));
+        break;
+    end;
+end;
+
